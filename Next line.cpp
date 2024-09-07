437549b9d63768965fdef334bd64b7bf1eeb8fa8
Divyanshi Solanki <161625343+Divyanshi-solanki@users.noreply.github.com>
2024-09-07 16:31:38 +0530

    Create Next line.cpp

---
+++ Next line.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  //using \n for line change //
+
+cout<<"This \nText \nWill \nBe \nPrinted \nIn \nDifferent \nLines";
+  
+return 0;
+}
